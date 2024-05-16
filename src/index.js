f08274ab9f27889f4881fd857237b19f3ffb36a9
kb603 <kojoaddobaffour@gmail.com>
2024-05-16 19:44:57 +0000

    update

--- src/index.js
+++ src/index.js
@@ -49,10 +49,48 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
       <Pizza />
     </div>
   );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  //   if (hour >= 12 && hour <= 22) {
+  //     alert("We are currently open");
+  //   } else {
+  //     alert("we are currently closed");
+  //   }
+
+  // For 12 hour time format
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  };
+  const currentTime = new Date().toLocaleTimeString("en-US", options);
+  return <footer>{currentTime}.We are currently open</footer>;
 }
 
 function Pizza() {
